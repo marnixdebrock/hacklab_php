c6b0bdee6c3480d72bff3a885140fbdf066668b1
Marnix de Brock <marnixx.debrock@gmail.com>
2024-09-16 17:52:57 +0200

    commentaar toegevoegd, levens op 10 gezet, en een test variable eruit gehaald.

--- basics.php
+++ basics.php
@@ -1,23 +1,25 @@
 <?php
-
-$lives = 3;
+// variablen
+$lives = 10;
 $userGuess = readline("Kies het maximale nummer: ");
+//input om een max nummer te kiezen
 $randomNumber = rand(0, $userGuess);
-echo $randomNumber;
 
+//loopt totdat de levens op zijn
 while ($lives > 0){
     $userGuess = readline("Vul een nummer in om te raden: ");
 
+    //kijkt of het antwoord onjuist is en geeft een bericht terug naar de speler, haalt ook 1 leven weg
     if ($userGuess != $randomNumber){
         $lives -= 1;
         echo "Helaas, je hebt hem niet geraden... je hebt nog " . $lives . " levens over. \n";
-
+        //kijkt of de levens op zijn, geef bericht terug naar speler en beëindigd de loop en spel
         if ($lives == 0){
             echo "Je hebt het juiste nummer niet geraden. " . "het juiste nummer was: " . $randomNumber;
             break;
         }
     }
-
+    //kijkt of het woord juist is, zowel, geeft bericht aan speler en beëindigd de loop en spel.
     if ($userGuess == $randomNumber){
         echo "Gefeliciteerd! Je hebt het nummer geraden!";
         break;
