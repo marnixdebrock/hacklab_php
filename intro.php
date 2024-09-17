cdcbbd5a7ff295638bdd43085d99f02b730395f9
marnixdebrock <marnixxdebrock@gmail.com>
2024-09-17 13:46:46 +0200

    commentaar toegevoegd, 'else' weggehaald bij eerste if statement en vervangen met de 'continue' statement

--- intro.php
+++ intro.php
@@ -1,15 +1,18 @@
 <?php
+//variablen
 $numberArray = array(42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8);
 $evenUnevenArray = array("even"=>0, "uneven"=>0);
 
-
+//looped totdat $numberArray geen items meer heeft
 foreach ($numberArray as $number){
+    //voegt nummer toe aan de 'even' waarde in $evenUnevenArray wanneer het nummer even is
     if ($number % 2 == 0){
         $evenUnevenArray['even'] += $number;
+        continue; 
     }
-    else{
-        $evenUnevenArray['uneven'] += $number;
-    }
+    //voegt nummer toe aan de 'uneven' waarde in $evenUnevenArray wanneer het nummer uneven is
+    $evenUnevenArray['uneven'] += $number;
+    
 }
 
 var_dump($evenUnevenArray);
