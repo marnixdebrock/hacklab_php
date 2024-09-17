a85cff34ba325bb4d09200b7cb4e1b5aba884211
marnixdebrock <marnixxdebrock@gmail.com>
2024-09-17 15:22:51 +0200

    added english comments, swapped the order of the if statements and moved the death message outside of the loop

--- basics.php
+++ basics.php
@@ -1,30 +1,27 @@
 <?php
-// variablen
+// variables
 $lives = 10;
-$userGuess = readline("Kies het maximale nummer: ");
-//input om een max nummer te kiezen
-$randomNumber = rand(0, $userGuess);
+$userNumber = readline("Kies het maximale nummer: ");
+//picks a random number between 0 and a number chosen by the player
+$randomNumber = rand(0, $userNumber);
 
-//loopt totdat de levens op zijn
+//loops until user runs out of lives
 while ($lives > 0){
     $userGuess = readline("Vul een nummer in om te raden: ");
 
-    //kijkt of het antwoord onjuist is en geeft een bericht terug naar de speler, haalt ook 1 leven weg
-    if ($userGuess != $randomNumber){
-        $lives -= 1;
-        echo "Helaas, je hebt hem niet geraden... je hebt nog " . $lives . " levens over. \n";
-        //kijkt of de levens op zijn, geef bericht terug naar speler en beëindigd de loop en spel
-        if ($lives == 0){
-            echo "Je hebt het juiste nummer niet geraden. " . "het juiste nummer was: " . $randomNumber;
-            break;
-        }
-    }
-    //kijkt of het woord juist is, zowel, geeft bericht aan speler en beëindigd de loop en spel.
+    //checks if number is correct, if so.. displays message for user and ends the game
     if ($userGuess == $randomNumber){
         echo "Gefeliciteerd! Je hebt het nummer geraden!";
         break;
     }
+    //if the answer is incorrect, it removes a life
+    $lives -= 1;
+    echo "Helaas, je hebt hem niet geraden... je hebt nog " . $lives . " levens over. \n";
 }
+    
+//displays message after running out of lives
+echo "Je hebt het juiste nummer niet geraden. " . "het juiste nummer was: " . $randomNumber;
+
 
 
 
